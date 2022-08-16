e3c401c50ea68aeaad83f52ab8f8fc6cd7dee263
dusann14 <dusan.stoimenovicvr@gmail.com>
2022-08-16 20:27:18 +0200

    pocetak

--- laptop_store/pages/start.js
+++ laptop_store/pages/start.js
@@ -1,15 +1,19 @@
 import Head from "next/head"
+import { useState } from "react";
+
 
 export default function Start() {
+
+
     return (
         <div>
             <Head>
                 <title>Razer</title>
-                <link rel="icon" href="/razer_logo.svg" />
+                <link rel="icon" href="/razer_logo.svg" / >
             </Head>
             <div>
-                <h1>naslov</h1>
-                <p>lorem ipsum dolor</p>
+               
+                
             </div>
         </div>
     )
