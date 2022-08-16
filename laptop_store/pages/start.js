6d2a8ca0be2d5746fa55612dfa67984dfddbcfc7
nikolas <nikola.stojilkovic6@gmail.com>
2022-08-16 19:00:09 +0200

    added starting page

---
+++ laptop_store/pages/start.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Start() {
+    return (
+        <div>
+            <Head>
+                <title>Razer</title>
+                <link rel="icon" href="/razer_logo.svg" />
+            </Head>
+            <div>
+                <h1>naslov</h1>
+            </div>
+        </div>
+    )
+}
