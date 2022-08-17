dd22a4804d7c031d2bdcf3fb2fa27f3aa04ba4c9
dusann14 <dusan.stoimenovicvr@gmail.com>
2022-08-17 22:57:44 +0200

    button on start page and laptop image

--- laptop_store/pages/start.js
+++ laptop_store/pages/start.js
@@ -5,11 +5,17 @@ import React, {useState, useEffect} from "react"
 
 const Start = () => {
 
-    const [text, setText] = useState('Welcome to our online store!!!')
-    const [textTag, setTag] = useState(<h1>{text}</h1>);
+    const [textTag, setTag] = useState(<h1 className={styles.heading}>Welcome to our online store!!!</h1>);
+    const [buttonTag, setButton] = useState(<button onClick={Animation} className = {styles.button1}><a>CLICK ME!!!</a></button>)
+    const [img, setImg] = useState(<img src="laptop_sa_senkom.svg" className={styles.laptop}></img>)
 
     function Animation(){
-        setTag(<h1 className={styles.fadeMove}>{text}</h1>)
+        setButton(<button className= {styles.move}><a>CLICK ME!!!</a></button>) 
+        setTag(<h1 className={styles.fadeMove}>Welcome to our online store!!!</h1>)                
+    }
+
+    function AnimationImg(){
+        setImg(<img src="laptop_sa_senkom.svg" className={styles.fromBottom}></img>)
     }
 
     return (
@@ -18,12 +24,14 @@ const Start = () => {
                 <title>Razer</title>
                 <link rel="icon" href="/razer_logo.svg" />
             </Head>
-            <div className={styles.body}>
-                <button><a>CLICK ME!!!</a></button>
+            <div className={styles.body}>    
                  <div className = {styles.section}>
-
 		             {textTag}
 	             </div>
+                 {buttonTag}
+            </div>
+            <div className={styles.section1}>            
+                {img}
             </div>
         </div>
     )
