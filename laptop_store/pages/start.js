d27abaa5c9a1171128f724b7b6031150381d37e9
dusann14 <dusan.stoimenovicvr@gmail.com>
2022-08-18 20:07:30 +0200

    start modification

--- laptop_store/pages/start.js
+++ laptop_store/pages/start.js
@@ -3,36 +3,59 @@ import styles from "../styles/Start.module.css"
 import React, {useState, useEffect} from "react"
 
 
+
 const Start = () => {
 
     const [textTag, setTag] = useState(<h1 className={styles.heading}>Welcome to our online store!!!</h1>);
-    const [buttonTag, setButton] = useState(<button onClick={Animation} className = {styles.button1}><a>CLICK ME!!!</a></button>)
-    const [img, setImg] = useState(<img src="laptop_sa_senkom.svg" className={styles.laptop}></img>)
+    const [buttonTag, setButton] = useState(<button  className = {styles.button1}><a>CLICK ME!!!</a></button>)
 
     function Animation(){
-        setButton(<button className= {styles.move}><a>CLICK ME!!!</a></button>) 
         setTag(<h1 className={styles.fadeMove}>Welcome to our online store!!!</h1>)                
-    }
-
-    function AnimationImg(){
-        setImg(<img src="laptop_sa_senkom.svg" className={styles.fromBottom}></img>)
-    }
-
+    } 
+    
     return (
         <div>
             <Head>
                 <title>Razer</title>
                 <link rel="icon" href="/razer_logo.svg" />
             </Head>
-            <div className={styles.body}>    
+            <div className={styles.body} onClick = {Animation}>    
                  <div className = {styles.section}>
 		             {textTag}
 	             </div>
-                 {buttonTag}
+                 
             </div>
-            <div className={styles.section1}>            
-                {img}
+            <br></br>
+            <div className={styles.section1}>   
+                <p className={styles.new}>THE NEW</p>  
+                <p className = {styles.center}>RAZER BLADE 15</p>
+                <br></br>
+                <h1 className = {styles.center}>POWER.PERFORMANCE.PERFECTION.</h1>  
+                <br></br>
+                <div className={styles.logos}>
+                    <img src = "icon-intel-core-i9.png"></img>
+                    <img src = "logo-nvidia-geforce-rtx.png"></img>
+                </div>
+                <br></br>
+                <div className= {styles.section1_1}> 
+                    <img src="laptop_sa_senkom.svg" className={styles.laptop}></img>
+                </div>      
+                <p className={styles.text}>Just when you thought a gaming laptop couldn’t be any more beastly—introducing the new Razer Blade 15, now available with the latest 12th Gen Intel® Core™ processor (14-core) and NVIDIA® GeForce RTX™ 30 Series Laptop GPUs for the most powerful gaming laptop graphics ever. With your choice of a Full HD 360Hz, QHD 240Hz (G-SYNC), or new UHD 144Hz display, enjoy unrivalled performance packed into the thinnest 15” RTX gaming laptop chassis ever.</p>
             </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>       
+            <div className={styles.frame}>
+                 <iframe className= {styles.iframe} width="560" height="315" src="https://www.youtube.com/embed/54_Zsng1uFs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div className={styles.section2}>
+                
+            </div>
+
         </div>
     )
 
